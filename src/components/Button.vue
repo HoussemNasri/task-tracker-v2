b47105b3b78a34e23c59f9db56b849b362a6384c
Houssem Nasri <housi.housi2015@gmail.com>
2022-01-31 00:25:23 +0100

    Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+   <button class="btn" :style="{ background: color }">{{ title }}</button>
+</template>
+
+<script>
+   export default {
+      name: "Button",
+      props: {
+         title: String,
+         color: String,
+      },
+   };
+</script>
+
+<style scoped>
+   button {
+      padding: 10px 16px;
+      border-radius: 5px;
+   }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+   <header>
+      <h1>{{ title }}</h1>
+      <Button title="Add Task" color="green"/>
+   </header>
+</template>
+
+<script>
+   import Button from "./Button.vue";
+
+   export default {
+      name: "Header",
+      props: {
+         title: {
+            type: String,
+            default: "Hello World",
+         },
+      },
+      components: {
+         Button,
+      },
+   };
+</script>
+
+<style scoped>
+   header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+   }
+   h1 {
+      color: rgb(6, 20, 39);
+   }
+</style>

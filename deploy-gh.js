7e8977a374bf5c613bf411986b49ab09067bb409
Houssem Nasri <housi.housi2015@gmail.com>
2022-02-01 20:16:53 +0100

    Update the deployment script to build for production instead of staging

--- deploy-gh.js
+++ deploy-gh.js
@@ -6,7 +6,7 @@ const fs = require("fs");
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("npm", ["run", "build"]);
+    await execa("npm", ["run", "build-prod"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
